95d7fceb399590df63f89030be8f40a8a2c6b8c2
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2011-10-20 14:58:20 +0300

    [proxy] prevent mocking global function

--- src/Fumocker/Proxy.php
+++ src/Fumocker/Proxy.php
@@ -34,11 +34,11 @@ class Proxy
 
     protected function setFunctionName($functionName)
     {
-        if (false == is_string($functionName)) {
+        if (false == \is_string($functionName)) {
             throw new \InvalidArgumentException(
-                sprintf('Invalid function name provided. should be string but `%s` provided', gettype($functionName)));
+                \sprintf('Invalid function name provided. should be string but `%s` provided', \gettype($functionName)));
         }
-        $functionName = trim($functionName);
+        $functionName = \trim($functionName);
         if (empty($functionName)) {
             throw new \InvalidArgumentException('Function name is empty');
         }
@@ -48,11 +48,11 @@ class Proxy
 
     protected function setNamespace($namespace)
     {
-        if (false == is_string($namespace)) {
-            throw new \InvalidArgumentException(sprintf(
-                    'Invalid namespace provided. should be string but `%s` provided', gettype($namespace)));
+        if (false == \is_string($namespace)) {
+            throw new \InvalidArgumentException(
+                \sprintf('Invalid namespace provided. should be string but `%s` provided', \gettype($namespace)));
         }
-        $namespace = trim($namespace);
+        $namespace = \trim($namespace);
         if (empty($namespace)) {
             throw new \LogicException(
                 'Namespace is empty. It is not possible create the function in global namespace. There is original one');
@@ -69,7 +69,7 @@ class Proxy
      */
     public function setCallback($callback)
     {
-        if (false == is_callable($callback)) {
+        if (false == \is_callable($callback)) {
             throw new \InvalidArgumentException('Invalid callback provided');
         }
 
@@ -82,14 +82,14 @@ class Proxy
     public function call()
     {
         if ($this->callback) {
-            return \call_user_func_array($this->callback, func_get_args());
+            return \call_user_func_array($this->callback, \func_get_args());
         }
 
         if (false == \function_exists($this->functionName)) {
             throw new \BadFunctionCallException(sprintf('The function `%s` is not exist in global namespace', $this->functionName));
         }
 
-        return call_user_func_array($this->functionName, func_get_args());
+        return \call_user_func_array($this->functionName, \func_get_args());
     }
 
     /**
