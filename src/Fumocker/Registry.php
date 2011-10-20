9c6c3550a8a77ab81e5668c835c9ab46937a9675
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2011-10-20 15:58:16 +0300

    [registry] registry singleton

--- src/Fumocker/Registry.php
+++ src/Fumocker/Registry.php
@@ -4,5 +4,36 @@ namespace Fumocker;
 
 class Registry
 {
+    /**
+     * @var Registry
+     */
+    protected static $instance;
 
+    /**
+     *
+     */
+    protected function __construct()
+    {
+
+    }
+
+    /**
+     * @return void
+     */
+    protected function __clone()
+    {
+
+    }
+
+    /**
+     * @static
+     *
+     * @throws \RuntimeException
+     *
+     * @return Registry
+     */
+    public static function getInstance()
+    {
+        return self::$instance ?: self::$instance = new self();
+    }
 }
--- tests/Fumocker/Tests/RegistryTest.php
+++ tests/Fumocker/Tests/RegistryTest.php
@@ -1,10 +1,65 @@
 <?php
 namespace Fumocker\Tests;
 
+use Fumocker\Registry;
+
 class RegistryTest extends \PHPUnit_Framework_TestCase
 {
-    public function testClassExists()
+    /**
+     * @return void
+     */
+    public function setUp()
     {
-        $this->assertTrue(class_exists('Fumocker\Registry'));
+        $classReflection = new \ReflectionClass('Fumocker\Registry');
+        $propertyReflection = $classReflection->getProperty('instance');
+
+        $propertyReflection->setAccessible(true);
+        $propertyReflection->setValue($classReflection, null);
+        $propertyReflection->setAccessible(false);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotAllowToInstantiateViaConstructor()
+    {
+        $reflectionClass = new \ReflectionClass('Fumocker\Registry');
+
+        $reflectionConstructor = $reflectionClass->getConstructor();
+        $this->assertInstanceOf('ReflectionMethod', $reflectionConstructor, 'Constructor method should be defined in the class');
+        $this->assertFalse($reflectionConstructor->isPublic(), 'The constructor method should not have public access');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotAllowToClone()
+    {
+        $reflectionClass = new \ReflectionClass('Fumocker\Registry');
+
+        $reflectionClone = $reflectionClass->getMethod('__clone');
+        $this->assertInstanceOf('ReflectionMethod', $reflectionClone, 'Clone method should be defined in the class');
+        $this->assertFalse($reflectionClone->isPublic(), 'The clone method should not have public access');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAllowToGetSingletonInstanceOfRegistry()
+    {
+        $registry = Registry::getInstance();
+
+        $this->assertInstanceOf('Fumocker\Registry', $registry);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAlwaysReturnTheSameInstance()
+    {
+        $registryOne = Registry::getInstance();
+        $registryTwo = Registry::getInstance();
+
+        $this->assertSame($registryOne, $registryTwo);
     }
 }
